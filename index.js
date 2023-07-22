ad58ac968277b0794af908d47c9828562c13b26c
rayhan60611 <pecon.alok@gmail.com>
2023-07-22 18:52:58 +0600

    send json users data

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const usersData = require("./data/users.json");
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/users", (req, res) => {
+  res.send(usersData);
+});
+
 app.listen(port, () => {
-  console.log(`1st express server is listening on port ${port}`);
+  console.log(`1st express server is running on port ${port}`);
 });
